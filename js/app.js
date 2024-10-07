fac38a95105677ac35bb7edff3b8083a86377abb
EduardoMedina24 <eduardolevm.15@gmail.com>
2024-10-07 09:31:15 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var edad = prompt("Escriba su edad ")
+var ciudad = "Quito"
+var nombre = prompt("Escriba su Nombre ")
+
+console.log("Ejercicico  JS")
+
+
+console.log("Tu nombre es: " + nombre + " su edad es  " + edad + " la ciudad es: " + ciudad)
+
+document.write("Tu edad es: " + edad + " esta grande " + nombre + "el acudiente es: " + nombreAcudiente)
+
+alert(nombre + "tienes "+ edad + " vives  en " + ciudad) 
